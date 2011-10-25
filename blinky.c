0f33d73d0c71317734069dca76984c93d6331050
Richard Ramsden <richard@rramsden.ca>
2011-10-24 22:27:21 -0700

    fix indentation

--- blinky.c
+++ blinky.c
@@ -18,10 +18,10 @@ int main(void)
   CPU_PRESCALE( CPU_16MHz );
   LED_CONFIG;
 
-	while (1) {
+  while (1) {
     LED_ON;
-		_delay_ms(1000);
+    _delay_ms(1000);
     LED_OFF;
-		_delay_ms(1000);
-	}
+    _delay_ms(1000);
+  }
 }
